ca3bf6acd096c98131a242bf8f4426a48245ba4f
Hauntologist <virgil.mayreder@gmail.com>
2025-09-17 17:22:36 -0700

    .

---
+++ PhantomLimbs/About/HediffCompProperties_PsychicFocusDrain.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace PhantomLimbs
+{
+    public class HediffCompProperties_PsychicFocusDrain : HediffCompProperties
+    {
+        public float psyfocusDrainPerSecond = 0.005f; // Default value
+        public HediffCompProperties_PsychicFocusDrain() => compClass = typeof(HediffComp_PsychicFocusDrain);
+    }
+}
